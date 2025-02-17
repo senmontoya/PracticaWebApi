a99bc91a7bdc43a543e0cecc866bc325ad2a6148
micro <micro@MONTOYA>
2025-02-16 18:12:39 -0600

    Add project files.

---
+++ PracticaWebApi/Models/bibliotecaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+namespace PracticaWebApi.Models
+{
+    public class bibliotecaContext : DbContext
+    {
+        public bibliotecaContext(DbContextOptions<bibliotecaContext> options) : base(options)
+        {
+
+        }
+       
+    }
+}
